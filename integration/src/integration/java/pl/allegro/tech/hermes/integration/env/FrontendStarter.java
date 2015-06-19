2cae5386ccdf3bb692578054fd5f1ea1dc386d8c
Pawel Szymczyk <pawel.szymczyk@allegro.pl>
2015-06-19 17:11:02 +0200

    try ok ok client

--- integration/src/integration/java/pl/allegro/tech/hermes/integration/env/FrontendStarter.java
+++ integration/src/integration/java/pl/allegro/tech/hermes/integration/env/FrontendStarter.java
@@ -2,6 +2,9 @@ package pl.allegro.tech.hermes.integration.env;
 
 import com.jayway.awaitility.Duration;
 import com.mongodb.DB;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 import org.eclipse.jetty.client.HttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,9 +12,6 @@ import pl.allegro.tech.hermes.common.config.ConfigFactory;
 import pl.allegro.tech.hermes.common.config.Configs;
 import pl.allegro.tech.hermes.frontend.HermesFrontend;
 import pl.allegro.tech.hermes.test.helper.environment.Starter;
-
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeoutException;
 
 import static com.jayway.awaitility.Awaitility.await;
 
@@ -33,9 +33,9 @@ public class FrontendStarter implements Starter<HermesFrontend> {
     public void start() throws Exception {
         LOGGER.info("Starting Hermes Frontend");
         hermesFrontend = HermesFrontend.frontend()
-            .withBinding(new FongoFactory().provide(), DB.class)
-            .withBinding(configFactory, ConfigFactory.class)
-            .build();
+                .withBinding(new FongoFactory().provide(), DB.class)
+                .withBinding(configFactory, ConfigFactory.class)
+                .build();
 
         hermesFrontend.start();
         waitForStartup();
@@ -60,11 +60,15 @@ public class FrontendStarter implements Starter<HermesFrontend> {
         final HttpClient httpClient = HttpClientFactory.create();
 
         await().atMost(Duration.TEN_SECONDS).until(() -> {
-            try {
-                return httpClient.GET(frontendUrl).getStatus() == 200;
-            } catch (InterruptedException | ExecutionException | TimeoutException exception) {
-                return false;
-            }
+            Request request14192 = new Request.Builder()
+                    .url(frontendUrl)
+                    .build();
+
+            OkHttpClient client = new OkHttpClient();
+
+            Response resp = client.newCall(request14192).execute();
+
+            return resp.code() == 200;
         });
     }
 }
